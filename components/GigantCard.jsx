d3595880cb43dccada1bf6dfbd47a90c0c171ccb
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2023-05-17 17:12:20 -0600

    inicio de la integracion de auth

--- components/GigantCard.jsx
+++ components/GigantCard.jsx
@@ -1,13 +1,17 @@
-import { StyleSheet, Text, View,TouchableHighlight } from 'react-native'
+import { StyleSheet, Text, View,TouchableHighlight, Image } from 'react-native'
 import React from 'react'
-
+import img from "../assets/11.png"
 const GigantCard = (props)=>{
     console.log(props)
   return (
     <TouchableHighlight style={styles.card} onPress={()=>{
         props.nav.navigate('Profile', {name: 'Jane'})
     }}>
-      <Text>GigantCard</Text>
+      <View style={styles.content}>
+       <Text>GigantCard</Text>
+      <Image source= {img} style={styles.img}/> 
+      </View>
+      
     </TouchableHighlight>
   )
 }
@@ -19,6 +23,19 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         margin: 10,
         borderRadius: 10,
-        padding: 10
+        padding: 10,
+        display: "flex",
+        justifyContent: "space-around",
+    },
+    content:{
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center"
+      
+    },
+    img:{
+      width: 100,
+      height: 130
     }
 })
--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,19 +1,53 @@
 import { StyleSheet, Text, View, Button, TouchableHighlight, Alert } from 'react-native'
+// import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
 import React from 'react'
-import { useState } from 'react'
 import { SET_SESION } from '../redux/user.slice'
 import { useDispatch, useSelector } from 'react-redux'
+
+// GoogleSignin.configure({
+//     scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+//     webClientId: '<FROM DEVELOPER CONSOLE>', // client ID of type WEB for your server (needed to verify user ID and offline access)
+//     offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+//     hostedDomain: '', // specifies a hosted domain restriction
+//     forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+//     accountName: '', // [Android] specifies an account name on the device that should be used
+//     iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+//     googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+//     openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+//     profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+//   })
+
 const Login = () => {
     const user = useSelector(state => state.user.value)
   const dispatch = useDispatch()
-    const alerta = ()=> {
-        console.log(user)
-        console.log("click")
+    const alerta = async()=> {
+        // try {
+        //     await GoogleSignin.hasPlayServices();
+        //     const userInfo = await GoogleSignin.signIn();
+        //     this.setState({ userInfo });
+        //   } catch (error) {
+        //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        //       // user cancelled the login flow
+        //     } else if (error.code === statusCodes.IN_PROGRESS) {
+        //       // operation (e.g. sign in) is in progress already
+        //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        //       // play services not available or outdated
+        //     } else {
+        //       // some other error happened
+        //     }
+        //   }
     dispatch(SET_SESION(true))
   }  
   return (
     <View style={styles.loginContainer}>
         <View style={styles.textContainer}>
+            {/* <GoogleSigninButton
+  style={{ width: 192, height: 48 }}
+  size={GoogleSigninButton.Size.Wide}
+  color={GoogleSigninButton.Color.Dark}
+  onPress={alerta}
+  disabled={false}
+/> */}
             <Text style={styles.textStyles}>Login</Text>  
             <Button
             style={styles.botton} 
@@ -23,6 +57,7 @@ const Login = () => {
             <TouchableHighlight style={styles.botton} onPress={alerta}>
                 <Text>Iniciar Sesion</Text>
             </TouchableHighlight>
+            
         </View>
 
     </View>
